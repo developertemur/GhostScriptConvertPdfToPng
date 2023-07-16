c9cb5c51285f746f042daa11f8234a097b04ef8e
ganiyevtemur1 <ganiyevtemur1@gmail.com>
2023-07-16 10:16:53 +0500

    Initial

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//This project require http://www.ghostscript.com/download/gsdnld.html installer
+
+using System.Drawing.Imaging;
+using Ghostscript.NET.Rasterizer;
+
+string BaseDirectory = Environment.CurrentDirectory;
+string path = Path.Combine(BaseDirectory, "test.pdf");
+string path2 = Path.Combine(BaseDirectory, "test.png");
+
+GhostscriptRasterizer rasterizer=new GhostscriptRasterizer();
+rasterizer.Open(path);
+var image= rasterizer.GetPage(100,1);
+image.Save(path2,ImageFormat.Png);
+
+Console.WriteLine($"{path2} file generated.");
+
